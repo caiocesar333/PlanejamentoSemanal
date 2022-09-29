dbe6d94c420f03f2b1837dd11631b94ba6ea97a6
Caio <81143531+caiocesar333@users.noreply.github.com>
2022-09-29 12:59:02 -0300

    Adicionando o script do relogio

---
+++ script-timer.js
@@ -0,0 +1,24 @@
+function relogio(){
+    var data=new Date();
+    var hor=data.getHours();
+    var min=data.getMinutes();
+    
+    if(hor < 10){
+        hor="0"+hor;
+    }
+    if(min < 10){
+        min="0"+min;
+    }
+    
+    
+    var horas=hor + ":" + min;
+    
+
+    const h1 = document.createElement("h1");
+    h1.innerText = horas;
+    const currentTime = document.getElementById("current-time");
+    currentTime.appendChild(h1);
+
+}
+
+// var timer=setInterval(relogio,1000); 
